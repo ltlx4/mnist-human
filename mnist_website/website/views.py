35d0d25ae10f2e906457fa95bac35846cf878e0d
ltlx <ltlx4@icloud.com>
2021-10-28 14:36:03 +0200

    Testing csv from static file

--- mnist_website/website/views.py
+++ mnist_website/website/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render 
+import csv
 # Create your views here.
 
 
 def index(request):
+    return render(request, 'website/index.html')
+
+def test(request):
+    file = open('static/csv/mnist_test.csv')
+    csvreader = csv.reader(file)
+    # header = next(csvreader)
+    # print(header)
+    # rows = []
+    # for row in csvreader:
+    #     rows.append(row)
+    # return print(rows)
     return render(request, 'website/index.html')
