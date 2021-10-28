7b0dd277a4615904bda4cb77a11d4b60bb3e8998
ltlx <ltlx4@icloud.com>
2021-10-28 14:33:01 +0200

    Modified database models

--- mnist_website/website/models.py
+++ mnist_website/website/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
 # Create your models here.
-class Digit(models.Model):
+class User(models.Model):
+    class Meta:
+        verbose_name = 'Participant'
+        verbose_name_plural = 'Participants'
+        
+    name = models.CharField(max_length=100, null=False, blank=False)
+    degree = models.CharField(max_length=150, blank=False)
+    age = models.IntegerField(blank=False, null=False)
+    region = models.CharField(max_length=100, blank=False, null=False)
     image = models.ImageField(upload_to='images')
-    result = models.CharField(max_length=2, blank=True)
+    result = models.BooleanField(default=None)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
