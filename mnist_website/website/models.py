57c492469d48d3b60221e036ae03ab6d5ba489c6
ltlx <ltlx4@icloud.com>
2021-10-29 14:02:18 +0200

    Created new multipleImages class

--- mnist_website/website/models.py
+++ mnist_website/website/models.py
@@ -9,14 +9,30 @@ class User(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
     degree = models.CharField(max_length=150, blank=False)
     age = models.IntegerField(blank=False, null=False)
-    region = models.CharField(max_length=100, blank=False, null=False)
-    image = models.ImageField(upload_to='images')
+    region = models.CharField(max_length=20, blank=False, default='Europe') 
     result = models.BooleanField(default=None)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return str(self.id)
+        return self.name
 
     # def save(self, *args, **kwargs):
     #     return super.save(*args, **kwargs)
+
+class UserImage(models.Model):
+    class Meta:
+        verbose_name = 'Participant Image'
+        verbose_name_plural = 'Participants Images'
+    user = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
+    image = models.FileField(upload_to='images')
+    human_guess = models.IntegerField()
+    real_value = models.IntegerField()
+    correct_guess = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "{}'s guess ({}) ".format(str(self.user),str(self.id))
+
+    @property
+    def check_correct(self):
+        return True if self.human_guess == self.real_value else False
