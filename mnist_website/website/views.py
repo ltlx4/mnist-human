e25d06a102af638e13a01bc7531f1d220f1e33bb
ltlx <ltlx4@icloud.com>
2021-10-31 13:44:54 +0100

    define get_shuffled_samples, home, get_ip and get_image functions

--- mnist_website/website/views.py
+++ mnist_website/website/views.py
@@ -1,11 +1,47 @@
-from django.shortcuts import render 
+from django.shortcuts import render, redirect
 from keras.datasets import mnist
+from .forms import UserForm
+import numpy as np
+from PIL import Image
+import random
+from django.contrib.gis.geoip2 import GeoIP2
+import matplotlib.pyplot as plt
 
+
+
+def get_shuffled_samples():
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    images = x_train
+    labels = y_train
+    images_with_labels = list(zip(images,labels))
+    random.shuffle(images_with_labels)
+    return images_with_labels[:15]
 
 def home(request):
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
-    print('X_train: ' + str(x_train.shape))
-    print('Y_train: ' + str(y_train.shape))
-    print('X_test:  '  + str(x_test.shape))
-    print('Y_test:  '  + str(y_test.shape))
-    return render(request, 'website/index.html')
+    images, labels = zip(*get_shuffled_samples())
+    user_ip = '176.63.3.126'
+    g = GeoIP2()
+    country = g.city(user_ip)
+    context ={
+        'form': UserForm(),
+        'country': country['country_code'],
+        'continent': country['continent_name'],
+        'images': images,
+        'labels': labels,
+        'userlist': {1,2,3}
+    }
+    return render(request, 'website/index.html', context)    
+
+
+def get_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+def gen_image(arr):
+    two_d = (np.reshape(arr, (28, 28)) * 255).astype(np.uint8)
+    img = Image.fromarray(two_d, 'L')
+    return img
