8456904b1b10db97d1b952a484a48262a75d34f3
ltlx <ltlx4@icloud.com>
2021-11-02 11:08:34 +0100

    Removed normal funtions into utils.py

--- mnist_website/website/views.py
+++ mnist_website/website/views.py
@@ -1,27 +1,15 @@
-from django.shortcuts import render, redirect
-from keras.datasets import mnist
-from .forms import UserForm
-import numpy as np
-from PIL import Image
-import random
+from django.shortcuts import render, redirect       
 from django.contrib.gis.geoip2 import GeoIP2
-import matplotlib.pyplot as plt
+from .utils import *
+from .forms import UserForm
 
-
-
-def get_shuffled_samples():
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
-    images = x_train
-    labels = y_train
-    images_with_labels = list(zip(images,labels))
-    random.shuffle(images_with_labels)
-    return images_with_labels[:15]
 
 def home(request):
-    images, labels = zip(*get_shuffled_samples())
+    images, labels = zip(*shuffle_samples())
     user_ip = '176.63.3.126'
     g = GeoIP2()
     country = g.city(user_ip)
+    
     context ={
         'form': UserForm(),
         'country': country['country_code'],
@@ -41,7 +29,5 @@ def get_ip(request):
         ip = request.META.get('REMOTE_ADDR')
     return ip
 
-def gen_image(arr):
-    two_d = (np.reshape(arr, (28, 28)) * 255).astype(np.uint8)
-    img = Image.fromarray(two_d, 'L')
-    return img
+
+
