d6d92e30d8bed738bf006cf619579752410986c7
ltlx <ltlx4@icloud.com>
2021-11-08 11:24:53 +0100

    add save form functions to urls

--- mnist_website/website/urls.py
+++ mnist_website/website/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='index'),
+    path('', views.home, name='home'),
+    path('saveform', views.save_form_views, name='save_user_form')
 
 ]
