46cb8aeb6ac8e6465907a2dee96a45b8f668e680
ltlx <ltlx4@icloud.com>
2021-11-10 23:26:19 +0100

    Connect form views to urls.py

--- mnist_website/website/urls.py
+++ mnist_website/website/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
-    path('saveform', views.save_form_views, name='save_user_form')
+    path('saveform', views.UserCreateView.as_view(), name='save_user_form'),
+    path('create/', views.UserImageCreate.as_view(), name='create_user_image')
 
 ]
