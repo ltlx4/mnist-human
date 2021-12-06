f596a0e9bc0867bd8dfd72328fbc6e3b17c13bb7
kaanardasyn <82092510+kaanardasyn@users.noreply.github.com>
2021-12-06 02:02:10 +0100

    Add user validator to form class

--- mnist_website/website/forms.py
+++ mnist_website/website/forms.py
@@ -1,29 +1,44 @@
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
-from django.forms.widgets import Textarea 
-from django.forms import * 
+
+from django.core.validators import RegexValidator
+from django import forms
 from django.forms.models import ModelForm 
 from .models import User, UserImage
+from django.forms import inlineformset_factory
+import re
 
 
+def username_valid(username):
+    pattern = re.compile('^[a-zA-Z0-9]([._-](?![._-])|[a-zA-Z0-9]){1,18}[a-zA-Z0-9]$')
+    return pattern.match(username)
 
 
-
-class UserForm(ModelForm):
+class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('name', 'degree', 'age')
+        fields = ('name', 'username','degree', 'age')
+    
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['name'].widget.attrs.update({'type': 'text', 'name': 'name', 'placeholder': 'Your name...', 'required': 'required'})
+        self.fields['username'].widget.attrs.update({'type': 'username', 'name': 'name', 'placeholder': 'Choose your unique username...', 'required': 'required'})
         self.fields['degree'].widget.attrs.update({'type': 'text', 'name': 'degree', 'placeholder': 'Degree...', 'required': 'required'})
         self.fields['age'].widget.attrs.update({'type': 'text', 'name': 'age', 'placeholder': 'Your age...', 'required': 'required'})
     
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if not username_valid(username):
+            raise forms.ValidationError(u'Invalid Username')
+        return username
 
-class UserImageForm(ModelForm):
+class UserImageForm(forms.ModelForm):
     
     class Meta:
         model = UserImage
-        fields = ('image', 'human_guess', 'real_value',)
+        fields = ('human_guess', 'image')
 
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['human_guess'].widget.attrs.update({'type': 'number', 'name': 'human_guess', 'placeholder': 'Your best guess...!'})
+        self.fields['human_guess'].required = False
